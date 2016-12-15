2cce3c7be0448cf9916e556ba8632455b5776617
ryanzhao1993 <ryanzhao1993@qq.com>
2016-12-15 22:09:49 +0800

    add style css

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import './index.css';
 
 class Footer extends Component {
     render(){
         return (
             <div>
-                <h1> 我是footer </h1>
+                <h1 className="footer"> 我是footer </h1>
             </div>
         );
     }
--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
 
-
+import './index.css';
 
 class App extends Component {
     render() {
         return (
-            <div>
+            <div className="app">
                 <Header />
                 <Footer />
             </div>
