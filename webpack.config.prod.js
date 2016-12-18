b327a97a30f6d176032fdedccf40b7d5e2b9f457
ryanzhao1993 <ryanzhao1993@qq.com>
2016-12-18 19:40:52 +0800

    add uglify

---
+++ webpack.config.prod.js
@@ -0,0 +1,54 @@
+var path = require('path');
+var webpack = require('webpack');
+var htmlWebpackPlugin = require('html-webpack-plugin');
+var openBrowserWebpackPlugin = require('open-browser-webpack-plugin');
+var uglifyPlugin = webpack.optimize.UglifyJsPlugin;
+
+var config = {
+    entry: path.resolve(__dirname, './src/index.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: 'dist',
+        inline: true,
+        port: 8080,
+        stats: {
+            colors: true
+        }
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                loader: 'style!css',
+                include: path.resolve(__dirname, 'src')
+            },
+            {
+                test: /\.less$/,
+                loader: 'style!css!less',
+                include: path.resolve(__dirname, 'src')
+            }
+        ]
+    },
+    plugins:[
+        new htmlWebpackPlugin({
+            title: '搭建前端工作流',
+            template: './src/index.html'
+        }),
+        new openBrowserWebpackPlugin({
+            url: 'http://localhost:8080'
+        }),
+        new uglifyPlugin({
+            compress: false
+        })
+    ]
+};
+
+module.exports = config;
